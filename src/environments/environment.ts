67cae3a94eaad9ca330908f7cb793c1c79d26ec2
Ramakrushna Mishra <ram2mishra@gmail.com>
2024-03-16 20:57:49 +0530

    final commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://cloudexplorers-openai.openai.azure.com/openai/deployments/cloudexplorers-bot/chat/completions?api-version=2024-02-15-preview',
+  apiUrl: 'https://cloudexplorers-openai.openai.azure.com/openai/deployments/cloudexplorers-bot/extensions/chat/completions?api-version=2023-08-01-preview',
   OPENAI_API_KEY: '17d1c2710aef466a964b9103d5b62e50',
+  searchKey: 'ALwJCbKZZpc4utICXlwpPpO4sevmEmsFu3SnPtkYmvAzSeCkrCqc',
 };
